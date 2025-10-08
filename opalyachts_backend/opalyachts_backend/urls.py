4f4e5177a1343d0f4bc160681114d4044deb84d1
Oussema Dammak <oussemadammak01@gmail.com>
2025-10-09 00:36:42 +0100

    fix Deployment

--- opalyachts_backend/opalyachts_backend/urls.py
+++ opalyachts_backend/opalyachts_backend/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('api/'),
     path('admin/', admin.site.urls),
     path('api/properties/',include('property.urls')),
     path('api/auth/',include('useraccount.urls')),
