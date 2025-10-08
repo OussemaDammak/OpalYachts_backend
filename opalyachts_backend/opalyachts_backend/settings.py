0c471fa0c2bcdddbd7d643332324f9319447098a
Oussema Dammak <oussemadammak01@gmail.com>
2025-10-08 09:13:23 +0100

    fix Deployment images

--- opalyachts_backend/opalyachts_backend/settings.py
+++ opalyachts_backend/opalyachts_backend/settings.py
@@ -31,6 +31,13 @@ SESSION_COOKIE_SECURE = True
 # allauth settings / Render
 ACCOUNT_EMAIL_VERIFICATION = "none"  
 
+#store images 
+# .env
+CLOUDINARY_CLOUD_NAME=your_cloud_name
+CLOUDINARY_API_KEY=your_api_key
+CLOUDINARY_API_SECRET=your_api_secret
+
+
 
 
 AUTH_USER_MODEL='useraccount.User'
@@ -118,7 +125,9 @@ INSTALLED_APPS = [
     'chat',
     'useraccount',
     'property',
-    # deployment
+    # storing images
+    'cloudinary',
+    'cloudinary_storage',
 
 ]
 
